9fc2bb407c489720b7491e6f3d7b03c82f4bc1c7
tysonyu87 <taiyou940304@gmail.com>
2022-08-26 23:11:14 +0800

    Update GenshinLogLoader.cs

--- src/KeqingNiuza.Final/Core/Wish/GenshinLogLoader.cs
+++ src/KeqingNiuza.Final/Core/Wish/GenshinLogLoader.cs
@@ -18,7 +18,7 @@ public class GenshinLogLoader
         {
             matches = FindUrl(@"AppData\LocalLow\miHoYo\Genshin Impact\output_log.txt");
             if (matches is null || matches.Count == 0)
-                throw new Exception("没有找到祈愿记录网址，由於3.0機制遭到修改因此請自行前往關於頁面手動輸入祈愿记录网址");
+                throw new Exception("沒有找到祈願紀錄網址，由於3.0機制遭到修改因此請自行前往關於頁面手動輸入祈願紀錄網址");
         }
 
         return matches[matches.Count - 1].Value.Replace("OnGetWebViewPageFinish:", "");
